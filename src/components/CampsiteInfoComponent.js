85b92008621074d0a8af25da93597f994021e137
Rongna Huang <wbb520wing@gmail.com>
2022-02-03 15:44:33 -0800

    Add files via upload

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,184 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, ModalHeader, ModalBody, Modal, Button, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
+
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+function RenderCampsite({ campsite }) {
+  return (
+    <div class="col-md-5 m-1">
+      <FadeTransform in transformProps={{
+        exitTransform: 'scale(0.5) translateY(-50%)'
+      }}>
+      <Card>
+        <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardTitle>{campsite.name}</CardTitle>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+      </FadeTransform>
+    </div>
+  )
+}
+
+function RenderComments({ comments, postComment, campsiteId }) {
+  if (comments) {
+    return (
+      <div class="col-md-5 m-1">
+        <h4>Comments</h4>
+        <Stagger in>
+        {
+          comments.map(comment => {
+            return (
+              <Fade in key={comment.id}>
+              <div>
+                <p>
+                {comment.text} <br />
+                --{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                </p>
+              </div>
+              </Fade>
+            )
+          })
+        }
+        </Stagger>
+        <CommentForm campsiteId={campsiteId} postComment={postComment}/>
+      </div>
+
+    )
+  }
+  return <div />
+}
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false
+    }
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    })
+  }
+
+  handleSubmit(values){
+    this.toggleModal();
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text)
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+              <div className="form-group">
+                <Label htmlFor="rating" >Rating</Label>
+                <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </Control.select>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="author">Your Name</Label>
+                <Control.text model=".author" id="author" name="author" className="form-control"
+                validators={{
+                  required,
+                  minLength: minLength(2),
+                  maxLength: maxLength(15)}}/>
+                <Errors
+                 className="text-danger"
+                 model=".author"
+                 show="touched"
+                 component="div"
+                 messages={{
+                     required: 'Required',
+                     minLength: 'Must be at least 2 characters',
+                     maxLength: 'Must be 15 characters or less'
+                 }}
+                />
+              </div>
+              <div className="form-group">
+                <Label htmlFor="text">Comment</Label>
+                <Control.textarea model=".text" id="text" name="text"
+                  rows="6" className="form-control" />
+              </div>
+              <Button type="submit" color="primary">Sumbit</Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+        <Button outline onClick={this.toggleModal} ><i className="fa fa-pencil" />Sumbit Comment</Button>
+      </div>
+    )
+  }
+}
+
+
+
+function CampsiteInfo(props) {
+  if(props.isLoading){
+    return(
+      <div className="container">
+          <div className="row">
+              <Loading />
+          </div>
+      </div>
+    )
+  }
+
+  if(props.errMess){
+    return(
+      <div className="container">
+          <div className="row">
+              <div className="col">
+                  <h4>{props.errMess}</h4>
+              </div>
+          </div>
+      </div>
+    )
+  }
+
+  if (props.campsite) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Breadcrumb>
+              <BreadcrumbItem><Link to='/directory'>Directory</Link></BreadcrumbItem>
+              <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <h2>{props.campsite.name}</h2>
+            <hr />
+          </div>
+        </div>
+        <div class="row">
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.comments}
+          postComment={props.postComment}
+          campsiteId={props.campsite.id}/>
+        </div>
+      </div>)
+  }
+  return <div />
+}
+
+
+export default CampsiteInfo
---
+++ src/redux/configureStore.js
@@ -0,0 +1,25 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Campsites } from './campsites';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './form';
+
+export const ConfigureStore = () => {
+  const store = createStore(
+    combineReducers({
+      campsites: Campsites,
+      comments: Comments,
+      partners: Partners,
+      promotions: Promotions,
+      ...createForms({
+        feedbackForm: InitialFeedback
+      })
+    }),
+    applyMiddleware(thunk, logger)
+  )
+  return store
+}
